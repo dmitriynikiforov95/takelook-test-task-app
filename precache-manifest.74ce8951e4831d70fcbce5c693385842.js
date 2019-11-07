8f8a00cb4cd3409067dc2001f97d1c1959bd2344
Dmitriy Nikiforov <nikiforov.psy@gmail.com>
2019-11-08 01:56:26 +0300

    Updates

---
+++ precache-manifest.74ce8951e4831d70fcbce5c693385842.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd1acec58dfffafa25391c4487d82947",
+    "url": "/takelook-test-task-app/index.html"
+  },
+  {
+    "revision": "8658a542a83369c314b8",
+    "url": "/takelook-test-task-app/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "20975629c6db5b2203d1",
+    "url": "/takelook-test-task-app/static/css/main.ae729dd6.chunk.css"
+  },
+  {
+    "revision": "8658a542a83369c314b8",
+    "url": "/takelook-test-task-app/static/js/2.220695bc.chunk.js"
+  },
+  {
+    "revision": "20975629c6db5b2203d1",
+    "url": "/takelook-test-task-app/static/js/main.ebd76f43.chunk.js"
+  },
+  {
+    "revision": "aa1961f475b01c4f7609",
+    "url": "/takelook-test-task-app/static/js/runtime-main.6679cfc1.js"
+  }
+]);
